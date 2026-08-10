b0736963c2a4d41971b1787b347b7993d5887952
KristinaStefanac <kristina.stefanac123@gmail.com>
2026-08-10 14:42:57 +0200

    Dodana  WalkerCard komponenta

---
+++ src/components/WalkerCard.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+import StarRating from './StarRating.vue'
+
+defineProps({
+  walker: { type: Object, required: true },
+})
+</script>
+
+<template>
+  <article class="card">
+    <h3>
+      <RouterLink :to="`/walkers/${walker.id}`">
+        {{ walker.firstName }} {{ walker.lastName }}
+      </RouterLink>
+    </h3>
+    <div class="muted">{{ walker.location }}</div>
+    <div>{{ (walker.services || []).join(' · ') }}</div>
+    <StarRating :stars="walker.averageRating" :count="walker.reviewCount" />
+    <div class="actions">
+      <RouterLink :to="`/walkers/${walker.id}`">
+        <button class="secondary">View profile</button>
+      </RouterLink>
+    </div>
+  </article>
+</template>
