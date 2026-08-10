bf06609a91fc3d3249045c4e5af0d7bd1d465741
KristinaStefanac <kristina.stefanac123@gmail.com>
2026-08-10 13:47:50 +0200

    Dodana koponenta SearchPanel

---
+++ src/components/SearchPanel.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps ({
+    modelValue: { type: String, default: '' },
+    placeholder: { type: String, default: 'City or area...'}  
+})
+defineEmits('update:modelValue', 'submit')
+</script>
+
+<template>
+    <from class="search.row" @submit.prevent="$emit('submit')">
+        <input
+            :value="modelValue"
+            :placeholder="placeholder"
+            @input="$emit('update:modelValue', $event.target.value)"        />
+        <button type="submit">Search</button>
+    </from>
+</template>
