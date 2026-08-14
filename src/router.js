4880635feda2f019372b18b1d1b17325c3c27de5
KristinaStefanac <kristina.stefanac123@gmail.com>
2026-08-14 23:22:05 +0200

    Dodana funkcionalnost Walker Inboxa

--- src/router.js
+++ src/router.js
@@ -4,6 +4,8 @@ import WalkerDetailView from './views/WalkerDetailView.vue'
 import ReserveView from './views/ReserveView.vue'
 import ReviewView from './views/ReviewView.vue'
 import BecomeWalkerView from './views/BecomeWalkerView.vue'
+import WalkerInboxView from './views/WalkerInboxView.vue'
+
 
 
 export default createRouter({
@@ -13,6 +15,7 @@ export default createRouter({
     { path: '/walkers/:id', name: 'walker', component: WalkerDetailView },
     { path: '/walkers/:id/reserve', name: 'reserve', component: ReserveView },
     { path: '/review', name: 'review', component: ReviewView },
-    { path: '/become-walker', name: 'become-walker', component: BecomeWalkerView }
+    { path: '/become-walker', name: 'become-walker', component: BecomeWalkerView },
+    {path: '/walker-inbox', name: 'walker-inbox', component: WalkerInboxView }
   ],
 })
