9f6f2c6265e8c1b3477667297acfa2ac002e9d92
KristinaStefanac <kristina.stefanac123@gmail.com>
2026-08-17 21:00:31 +0200

    Glavna navigacija

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from 'vue-router'
+import { useStubs } from './api/client.js'
 </script>
 
 <template>
-  <HelloWorld />
+  <nav class="nav">
+    <RouterLink to="/" class="brand">Walk Dog</RouterLink>
+    <RouterLink to="/">Find walkers</RouterLink>
+    <RouterLink to="/review">Write a review</RouterLink>
+    <RouterLink to="/become-walker">Become dog walker</RouterLink>
+    <RouterLink to="/walker-inbox">Walker inbox</RouterLink>
+    <span class="spacer" />
+    <span v-if="useStubs" class="badge">stub mode</span>
+  </nav>
+  <main class="container">
+    <RouterView />
+  </main>
 </template>
